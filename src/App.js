3285830b57b9d3b8a87742693be252b6ce1ba05e
Sanket3007 <karanjesanket1980@gmail.com>
2022-01-03 18:57:46 +0530

    First Page Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import MaterialTable from 'material-table';
+import "./app.css";
+import {Link} from '@material-ui/core'
+
 
 function App() {
+
+  const [data, setData] = useState([])
+  const columns = [
+    { title: "First Name", field: "first_name", searchable:true },
+    { title: "Last Name", field: "last_name", searchable:true },
+    { title: "Age", field: "age", searchable:false },
+    { title: "Email", field: "email", searchable:false },
+    { title: "Website", field:"web",render:rowData=><Link href={`${rowData.web}`} target="_blank">{rowData.web}</Link>, searchable:false}
+  ]
+  useEffect(() => {
+    fetch("https://datapeace-storage.s3-us-west-2.amazonaws.com/dummy_data/users.json")
+      .then(resp => resp.json())
+      .then(resp => {
+        setData(resp)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        Hello TVA!
-      </header>
+      <h1 align="center">Frontend Assignment (React.js)</h1>
+      <MaterialTable
+        title="Users"
+        data={data}
+        columns={columns}
+        options={{searchFieldAlignment: "left", searchFieldVariant: "outlined", pageSize: 10,pageSizeOptions:false,paginationCount:5 ,paginationType:"outline", showFirstLastPageButtons:false}}
+      />
     </div>
   );
 }
